aa40866b397883f6128c07e16527f4531d090e5b
shutnikPk <shutnikssspk@gmail.com>
2021-08-04 11:53:56 +0300

    fix according to advanses

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,11 +1,25 @@
 import DonateList from '../modules/donate-list'
 import DonateForm from '../modules/donate-form'
-import {getFormattedTime} from '../core/util/index'
+import*  as Utils from '../core/util/index'
+
+const     mockDonates = [
+    { amount: 4, date: new Date() },
+    { amount: 20, date: new Date() },
+    { amount: 3, date: new Date() },
+    { amount: 1, date: new Date() },
+  ];
+
 export default class App {
-        state={
-        donates:[],
-        totalAmount:0
-    }
+    constructor() {
+        this.state = {
+        donates: mockDonates,
+        totalAmount: 0,
+        }
+        this.state.totalAmount = Utils.calculateSumOfNumbers(this.state.donates.map((donate) => donate.amount));
+        this.donateForm = new DonateForm(this.state.totalAmount, this.createNewDonate.bind(this));
+        this.donateList = new DonateList(this.state.donates);
+        }
+
     createNewDonate(newDonate){
         this.state.donates.push(newDonate)
         this.state.totalAmount=this.donateList.getTotalAmount()
@@ -13,9 +27,7 @@ export default class App {
         this.donateList.updateDonates(this.state.donates)
         this.donateForm.updateTotalAmount(this.state.totalAmount)
     }
-    donateForm = new DonateForm(this.state.totalAmount,this.createNewDonate.bind(this))
 
-    donateList = new DonateList(this.state.donates)
     run(){
         this.donateForm.render()
         this.donateList.render()
@@ -26,7 +38,7 @@ export default class App {
             const donate={}
             donate.amount=Number(input.value)
             input.value=''
-            donate.date=getFormattedTime(new Date())
+            donate.date=Utils.getFormattedTime(new Date())
             this.donateForm.callback(donate)
         })
     }
